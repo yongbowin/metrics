fa9c2669fb1eb82c193ead02c6e294fa299649a1
yongbowin <yongbowin@outlook.com>
2019-08-15 17:31:04 +0800

    add annotion to rouge-l

--- rouge_bleu/utils/rouge_metric/rouge.py
+++ rouge_bleu/utils/rouge_metric/rouge.py
@@ -8,9 +8,9 @@
 # Author : Ramakrishna Vedantam <vrama91@vt.edu>
 
 import numpy as np
-import pdb
 
-def my_lcs(string, sub):
+
+def my_lcs(string, sub):  # (reference sentence tokens list, candidate sentence tokens list)
     """
     Calculates longest common subsequence for a pair of tokenized strings
     :param string : list of str : tokens from a string split using whitespace
@@ -18,26 +18,57 @@ def my_lcs(string, sub):
     :returns: length (list of int): length of the longest common subsequence between the two strings
 
     Note: my_lcs only gives length of the longest common subsequence, not the actual LCS
+
+    Example:
+        string = ['可', '以', '玩', '而', '已', '。'],
+        sub = ['可', '以', '玩', '而', '已', '。']
     """
-    if(len(string)< len(sub)):
+    if len(string) < len(sub):  # i.e., ensure `len(string)` > `len(sub)`
         sub, string = string, sub
 
-    lengths = [[0 for i in range(0,len(sub)+1)] for j in range(0,len(string)+1)]
+    """Example, 7 x 7
+    [
+        [0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0]
+    ]
+    """
+    lengths = [[0 for i in range(0, len(sub) + 1)] for j in range(0, len(string) + 1)]
 
-    for j in range(1,len(sub)+1):
-        for i in range(1,len(string)+1):
-            if(string[i-1] == sub[j-1]):
-                lengths[i][j] = lengths[i-1][j-1] + 1
+    print(lengths)
+
+    for j in range(1, len(sub) + 1):
+        for i in range(1, len(string) + 1):
+            if string[i - 1] == sub[j - 1]:
+                lengths[i][j] = lengths[i - 1][j - 1] + 1
             else:
-                lengths[i][j] = max(lengths[i-1][j] , lengths[i][j-1])
+                lengths[i][j] = max(lengths[i - 1][j], lengths[i][j - 1])
+
+    print(lengths)
+    """
+    [
+        [0, 0, 0, 0, 0, 0, 0], 
+        [0, 1, 1, 1, 1, 1, 1], 
+        [0, 1, 2, 2, 2, 2, 2], 
+        [0, 1, 2, 3, 3, 3, 3], 
+        [0, 1, 2, 3, 4, 4, 4], 
+        [0, 1, 2, 3, 4, 5, 5], 
+        [0, 1, 2, 3, 4, 5, 6]
+    ]
+    """
 
     return lengths[len(string)][len(sub)]
 
-class Rouge():
+
+class Rouge:
     '''
     Class for computing ROUGE-L score for a set of candidate sentences for the MS COCO test set
-
     '''
+
     def __init__(self):
         # vrama91: updated the value below based on discussion with Hovey
         self.beta = 1.2
@@ -49,27 +80,27 @@ class Rouge():
         :param refs: list of str : COCO reference sentences for the particular image to be evaluated
         :returns score: int (ROUGE-L score for the candidate evaluated against references)
         """
-        assert(len(candidate)==1)	
-        assert(len(refs)>0)         
+        assert (len(candidate) == 1)
+        assert (len(refs) > 0)
         prec = []
         rec = []
 
         # split into tokens
         token_c = candidate[0].split(" ")
-    	
+
         for reference in refs:
             # split into tokens
             token_r = reference.split(" ")
             # compute the longest common subsequence
             lcs = my_lcs(token_r, token_c)
-            prec.append(lcs/float(len(token_c)))
-            rec.append(lcs/float(len(token_r)))
+            prec.append(lcs / float(len(token_c)))
+            rec.append(lcs / float(len(token_r)))
 
         prec_max = max(prec)
         rec_max = max(rec)
 
-        if(prec_max!=0 and rec_max !=0):
-            score = ((1 + self.beta**2)*prec_max*rec_max)/float(rec_max + self.beta**2*prec_max)
+        if prec_max != 0 and rec_max != 0:
+            score = ((1 + self.beta ** 2) * prec_max * rec_max) / float(rec_max + self.beta ** 2 * prec_max)
         else:
             score = 0.0
         return score
@@ -82,21 +113,25 @@ class Rouge():
         :param ref_for_image: dict : reference MS-COCO sentences with "image name" key and "tokenized sentences" as values
         :returns: average_score: float (mean ROUGE-L score computed by averaging scores for all the images)
         """
-        assert(gts.keys() == res.keys())
+        assert (gts.keys() == res.keys())
         imgIds = gts.keys()
 
         score = []
         for id in imgIds:
             hypo = res[id]
-            ref  = gts[id]
+            ref = gts[id]
 
+            """
+            hypo = ['可 以 玩 而 已 。'], 
+            ref = ['可 以 玩 而 已 。']
+            """
             score.append(self.calc_score(hypo, ref))
 
             # Sanity check.
-            assert(type(hypo) is list)
-            assert(len(hypo) == 1)
-            assert(type(ref) is list)
-            assert(len(ref) > 0)
+            assert (type(hypo) is list)
+            assert (len(hypo) == 1)
+            assert (type(ref) is list)
+            assert (len(ref) > 0)
 
         average_score = np.mean(np.array(score))
         return average_score, np.array(score)
